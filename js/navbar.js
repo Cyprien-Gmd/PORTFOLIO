7f4c49f3a0381757bc4532b72aca3c82b13a995e
CYPRIEN <84139302+Cyprien-Gmd@users.noreply.github.com>
2021-06-07 21:44:27 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,19 @@
+// ICON BURGER TO CROSS
+function BurgerX(x) {
+  x.classList.toggle("change");
+  x.classList.toggle("closebtn");
+};
+
+
+// OUVERTURE ET FERMETURE NAV
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";    
-  }
+    document.getElementById("myNav").style.height = "100%";
+    if (height > 0) {
+      element.classList.remove("burgerbars");
+    }
+  };
   
   function closeNav() {
     document.getElementById("myNav").style.height = "0%";
-  }
+  };
+
